6d26debc66656bef1fa0e2a5e08f12f22e37db59
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-12-09 13:32:51 -0800

    Update git.py

--- setup/git.py
+++ setup/git.py
@@ -1,1 +1,7 @@
 from git import Repo
+
+def _create_head_master_from_origin(self):
+        if not 'master' in self.repo.heads:
+            self.logger.debug("Creating local master branch")
+            self.repo.create_head('master', self.origin.refs.master)
+        self.repo.heads.master.set_tracking_branch(self.origin.refs.master)
